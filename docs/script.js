db7f3ba666a538736aac94060ad8bb6de8425446
PeterSoSmart <107408955+PHP135@users.noreply.github.com>
2024-12-25 13:48:04 +0700

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,2 +1,7 @@
 var noButton = document.getElementById("no");
 noButton.addEventListener("click", function() {noButton.style.background = "blue";});
+
+
+
+var audio = document.getElementById('light');  
+  audio.volume = 0.2; // Set volume to 20%
